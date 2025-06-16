29264044100276ee1b3b663794646285f0253fd3
Cesar Hernandez <cehergra1999@gmail.com>
2025-06-16 12:41:45 -0500

    Add files via upload

---
+++ JS/01-tipos/02-primitivos.js
@@ -0,0 +1,8 @@
+let numero = 1; // number
+let texto = "Hola mundo"; //string
+let verdadero = true; //boolean
+let falso = false; // boolean
+let noDefinido; // undefined
+let nulo = null; // null
+
+
