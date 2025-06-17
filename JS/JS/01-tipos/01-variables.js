37c339278e149249729c3840b86fb56dd05e4697
Cesar Hernandez <cehergra1999@gmail.com>
2025-06-17 12:39:45 -0500

    Add files via upload

---
+++ JS/JS/01-tipos/01-variables.js
@@ -0,0 +1,9 @@
+let nombre = "Hola Mundo";
+let NombreCompleto="chancho";
+let nombreCompleto="felipe";
+let nombre_completo
+console.log(nombre);
+
+let apellido, animal
+apellido = "Hdez"
+animal = "Zebra"
---
+++ JS/JS/02-operadores/03-comparacion.js
@@ -0,0 +1,14 @@
+let a = 10;
+//relacionales
+console.log(a > 5);
+console.log(a >= 5);
+console.log(a < 5);
+console.log(a <= 5);
+
+//igualdad
+console.log(a == 10);//estos ya no se suelen usar porque dan error
+console.log(a != 10);//*
+console.log(a == "10");//*
+console.log(a === "10");
+console.log(a !== "10");
+
