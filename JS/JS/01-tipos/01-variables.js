d05b5845365d30c58d88de66beba744795d4c216
Cesar Hernandez <cehergra1999@gmail.com>
2025-06-17 12:40:32 -0500

    Delete JS/JS directory

--- JS/JS/01-tipos/01-variables.js
+++
@@ -1,9 +0,0 @@
-let nombre = "Hola Mundo";
-let NombreCompleto="chancho";
-let nombreCompleto="felipe";
-let nombre_completo
-console.log(nombre);
-
-let apellido, animal
-apellido = "Hdez"
-animal = "Zebra"
--- JS/JS/01-tipos/02-primitivos.js
+++
@@ -1,8 +0,0 @@
-let numero = 1; // number
-let texto = "Hola mundo"; //string
-let verdadero = true; //boolean
-let falso = false; // boolean
-let noDefinido; // undefined
-let nulo = null; // null
-
-
